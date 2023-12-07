a484956f5220847d5e475e80e08d32c5ae38dea6
Alfesh Abdul <alfeshabdul2002@gmail.com>
2023-12-07 13:33:03 +0530

    made a small change for i18n

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,31 @@
-import React from "react";
+
 import "./Home.css";
+import I18n from "../utils/i18n";
 
 function Home(){
+
+   const usersCount = 2;
+
     return (
-        <div> Home page</div>
+        <div className="container"> 
+            <h1>{I18n ("welcomeM")}</h1>
+            <p>{I18n ("introM")}</p>
+            <h3>{I18n ("joiningM", "<attend>", usersCount)}</h3>
+            <h2>{I18n ("moralM")}</h2>
+
+            <select 
+             defaultValue={localStorage.getItem('lang')}
+             onChange={(e) =>{
+                localStorage.setItem('lang', e.target.value);
+                window.location.reload();
+             }}>
+                <option value="en"> English</option>
+                <option value="hi"> Hindi</option>
+                <option value="mr"> Marathi</option>
+           </select>
+
+    
+        </div>
     )
 }
 
